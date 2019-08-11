fb94a9267421f51220460a144da35c39163acc24
sean davis <sean.davis1289@gmail.com>
2019-08-11 18:37:56 -0400

    add new weather data

---
+++ models/weather.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let WeatherSchema = new Schema({
+  weather_id: {type: Number, required: true },
+  date: {type: Date, required: true },
+  temperature: {type: Array, required: true },
+  location: {
+    lat: {type: Number },
+    lon: {type: Number },
+    city: {type: String },
+    state: {type: String }
+  }
+});
+
+module.exports = mongoose.model('Weather', WeatherSchema);
--- routes/weather.js
+++ routes/weather.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const Location = require('../models/location.model.js');
+const Weather = require('../models/weather.model.js');
 
-router.post('/create', function(req,res,next) {
-  let location = new Location(
-    {
-      lat: req.body.lat,
-      lon: req.body.lon,
-      city: req.body.city,
-      state: req.body.state
-    }
-  )
-  location.save(function(err) {
-    if (err) {
-      return next(err);
-    }
-    res.status(201).send(location);
-  })
-});
+router.post('/create', createWeather);
 
 module.exports = router;
+
+
+function createWeather(req, res, next) {
+  let weather = req.body;
+  console.log(weather.weather_id);
+  Weather.findOne({weather_id: weather.weather_id }, function(err, result) {
+    console.log(err);
+    if (err) next(err);
+    if (result) {
+      return res.status(400).send(`There already exists weather data with id: ${weather.weather_id}`);
+    }
+
+    let newWeather = new Weather(weather);
+    newWeather.save(function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.status(201).send(weather);
+    })
+  });
+};
